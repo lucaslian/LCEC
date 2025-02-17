ed581b2c398734337e11f01eaff0131805f7d573
kslian <76818181+lucaslian@users.noreply.github.com>
2025-02-17 08:50:26 -0600

    Second try

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './Components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import FacultyPictures from './Components/FacultyPictures';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { HashRouter as Router, Route, Routes} from 'react-router-dom';
 import About from './Navigation/About';
 import Contact from './Navigation/Contact';
 import Login from './Navigation/Login';
