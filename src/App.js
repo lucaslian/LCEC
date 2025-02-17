0d6cdc91307ed7ed0acfc9238e6d246bbd12393f
kslian <76818181+lucaslian@users.noreply.github.com>
2025-02-17 09:57:47 -0600

    router changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './Components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import FacultyPictures from './Components/FacultyPictures';
-import { HashRouter as Router, Route, Routes} from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import About from './Navigation/About';
 import Contact from './Navigation/Contact';
 import Login from './Navigation/Login';
@@ -14,20 +14,20 @@ function App() {
       <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
               <div class="container-fluid">
-                <a class="navbar-brand" href="/">LCEC</a>
+                <Link class="nav-link" to="/">LCEC</Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                      <a class="nav-link" href="/faculty-pictures">Faculty</a>
+                      <Link class="nav-link" to="/faculty-pictures">Faculty</Link>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" aria-disabled="false" href="/about">About</a>
+                      <Link class="nav-link" to="/about">About</Link>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" aria-disabled="false" href="/Contact">Contact</a>
+                      <Link class="nav-link" aria-disabled="false" to="/Contact">Contact</Link>
                     </li>
                     <li class="nav-item">
                       
